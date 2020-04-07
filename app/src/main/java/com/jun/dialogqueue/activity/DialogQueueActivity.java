d325fb37458f0f251805d2c05e87c04c7d15f90b
liujun <liujun@dachentech.com.cn>
2020-04-07 19:05:49 +0800

    优化

--- app/src/main/java/com/jun/dialogqueue/activity/DialogQueueActivity.java
+++ app/src/main/java/com/jun/dialogqueue/activity/DialogQueueActivity.java
@@ -8,18 +8,25 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.jun.dialogqueue.R;
 import com.jun.dialogqueue.util.DialogQueue;
+import com.jun.dialogqueue.util.DialogQueue.Element;
+import com.jun.dialogqueue.util.DialogQueue.Type;
 
-public class DialogQueueActivity extends AppCompatActivity implements DialogQueue.PopUpListener {
+import static com.jun.dialogqueue.util.DialogQueue.Type.TYPE1;
+import static com.jun.dialogqueue.util.DialogQueue.Type.TYPE2;
+import static com.jun.dialogqueue.util.DialogQueue.Type.TYPE3;
+import static com.jun.dialogqueue.util.DialogQueue.Type.TYPE4;
+import static com.jun.dialogqueue.util.DialogQueue.Type.TYPE5;
+import static com.jun.dialogqueue.util.DialogQueue.Type.TYPE6;
+import static com.jun.dialogqueue.util.DialogQueue.Type.TYPE7;
+
+public class DialogQueueActivity extends AppCompatActivity implements DialogQueue.PopupListener {
 
     private DialogQueue queue;
-
-    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_queue);
-        handler = new Handler();
         queue = new DialogQueue(this);
         getType1();
         getType2();
@@ -34,70 +41,63 @@ public class DialogQueueActivity extends AppCompatActivity implements DialogQueu
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogQueue.Element element = new DialogQueue.Element(queue.types[0], null);
-                queue.add(element);
+                queue.enqueue(new Element(TYPE1, null));
             }
-        }, 3000);
+        }, 4000);
     }
 
     private void getType2() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogQueue.Element element = new DialogQueue.Element(queue.types[1], null);
-                queue.add(element);
+                queue.enqueue(new Element(TYPE2, false, null));
             }
-        }, 3000);
+        }, 7000);
     }
 
     private void getType3() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogQueue.Element element = new DialogQueue.Element(queue.types[2], null);
-                queue.add(element);
+                queue.enqueue(new Element(TYPE3, null));
             }
-        }, 6000);
+        }, 3000);
     }
 
     private void getType4() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogQueue.Element element = new DialogQueue.Element(queue.types[3], null);
-                queue.add(element);
+                queue.enqueue(new Element(TYPE4, false,null));
             }
-        }, 1000);
+        }, 8000);
     }
 
     private void getType5() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogQueue.Element element = new DialogQueue.Element(queue.types[4], null);
-                queue.add(element);
+                queue.enqueue(new Element(TYPE5, null));
             }
-        }, 4000);
+        }, 6000);
     }
 
     private void getType6() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogQueue.Element element = new DialogQueue.Element(queue.types[5], null);
-                queue.add(element);
+                queue.enqueue(new Element(TYPE6, null));
             }
-        }, 11000);
+        }, 1000);
     }
 
     private void getType7() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                DialogQueue.Element element = new DialogQueue.Element(queue.types[6], null);
-                queue.add(element);
+                queue.enqueue(new Element(TYPE7, null));
             }
-        }, 5000);
+        }, 10000);
     }
 
     private AlertDialog showDialog(String message) {
@@ -122,62 +122,65 @@ public class DialogQueueActivity extends AppCompatActivity implements DialogQueu
     }
 
     @Override
-    public void onPopUp(final DialogQueue.Element element) {
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                final DialogQueue.Type type = element.getType();
-                if (type.orderValue == queue.types[0].orderValue) {
-                    showDialog("1").setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            queue.finish(type);
-                        }
-                    });
-                } else if (type.orderValue == queue.types[1].orderValue) {
-                    showDialog("2").setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            queue.finish(element.getType());
-                        }
-                    });
-                } else if (type.orderValue == queue.types[2].orderValue) {
-                    showDialog("3").setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            queue.finish(type);
-                        }
-                    });
-                } else if (type.orderValue == queue.types[3].orderValue) {
-                    showDialog("4").setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            queue.finish(type);
-                        }
-                    });
-                } else if (type.orderValue == queue.types[4].orderValue) {
-                    showDialog("5").setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            queue.finish(type);
-                        }
-                    });
-                } else if (type.orderValue == queue.types[5].orderValue) {
-                    showDialog("6").setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            queue.finish(type);
-                        }
-                    });
-                } else if (type.orderValue == queue.types[6].orderValue) {
-                    showDialog("7").setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            queue.finish(type);
-                        }
-                    });
-                }
-            }
-        }, 500);
+    public void onPopup(Element element) {
+        final Type type = element.type;
+        switch (type) {
+            case TYPE1:
+                showDialog("1").setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        queue.dequeue(type);
+                    }
+                });
+                break;
+            case TYPE2:
+                showDialog("2").setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        queue.dequeue(type);
+                    }
+                });
+                break;
+            case TYPE3:
+                showDialog("3").setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        queue.dequeue(type);
+                    }
+                });
+                break;
+            case TYPE4:
+                showDialog("4").setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        queue.dequeue(type);
+                    }
+                });
+                break;
+            case TYPE5:
+                showDialog("5").setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        queue.dequeue(type);
+                    }
+                });
+                break;
+            case TYPE6:
+                showDialog("6").setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        queue.dequeue(type);
+                    }
+                });
+                break;
+            case TYPE7:
+                showDialog("7").setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        queue.dequeue(type);
+                    }
+                });
+                break;
+        }
     }
 }
--- app/src/main/java/com/jun/dialogqueue/util/DialogQueue.java
+++ app/src/main/java/com/jun/dialogqueue/util/DialogQueue.java
@@ -1,79 +1,100 @@
 package com.jun.dialogqueue.util;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class DialogQueue {
 
-    public Type[] types = new Type[]{
-            new Type("TYPE1", true, 0),
-            new Type("TYPE2", true, 1),
-            new Type("TYPE3", false, 2),
-            new Type("TYPE4", false, 3),
-            new Type("TYPE5", false, 4),
-            new Type("TYPE6", false, 5),
-            new Type("TYPE7", false, 6),
-    };
+    public enum Type {
+
+        TYPE1(true),
+        TYPE2(true),
+        TYPE3(true),
+        TYPE4(true),
+        TYPE5,
+        TYPE6,
+        TYPE7;
 
 
-    public DialogQueue(PopUpListener popUpListener) {
-        this.popUpListener = popUpListener;
-        for (int i = 0; i < types.length; i++) {
-            if (types[i].isOrder) orderExecuteNum++;
+        public boolean isOrder;//是否是顺序弹窗,顺序弹窗定义顺序决定弹出顺序
+
+        Type(boolean isOrder) {
+            this.isOrder = isOrder;
         }
+
+        Type() {
+        }
+    }
+
+    public DialogQueue(PopupListener popupListener) {
+        for (Type type : Type.values()) {
+            if (type.isOrder) {
+                orderedQueue.add(null);//初始状态,以区分element不为空,element不为空表明已确定是否需要弹出
+                orderedCount++;
+            }
+        }
+        this.popupListener = popupListener;
     }
 
     /**
      * 弹窗弹出监听
      */
-    private PopUpListener popUpListener;
-    /**
-     * 顺序弹窗数量
-     */
-    volatile private int orderExecuteNum;
+    private PopupListener popupListener;
     /**
      * 顺序弹窗列表
      */
-    private List<Element> orderExecuteList = new ArrayList<>();
-    /**
-     * 顺序弹窗排序
-     */
-    private OrderListComparator comparator = new OrderListComparator();
+    private List<Element> orderedQueue = new ArrayList<>();
     /**
      * 非顺序弹窗列表
      */
-    private List<Element> unOrderExecuteList = new ArrayList<>();
+    private List<Element> unorderedQueue = new ArrayList<>();
     /**
-     * 当前正在显示的弹窗元素
+     * 当前正在显示的弹窗元素(防止正在显示弹窗还未关闭,enqueue后重复显示)
      */
     private Element currentElement;
     /**
      * 非顺序弹窗列表是否可以开始弹出
      */
-    private boolean canUnOrderExe = false;
-
+    private boolean canUnorderedPopup = false;
+    /**
+     * 顺序弹窗数量
+     */
+    private int orderedCount;
 
     /**
      * 添加弹窗到队列
      *
      * @param element 弹窗元素
      */
-    public void add(Element element) {
+    public void enqueue(Element element) {
         if (element.type.isOrder) {//顺序弹窗需要弹出时添加
-            synchronized (orderExecuteList) {
-                orderExecuteList.add(element);
-                if (orderExecuteList.size() == orderExecuteNum) {
-                    Collections.sort(orderExecuteList, comparator);
-                    popupOrder();
+            if (orderedQueue.size() > element.type.ordinal() && orderedQueue.get(element.type.ordinal()) == null) {
+                synchronized (orderedQueue) {
+                    orderedQueue.set(element.type.ordinal(), element);
+//                    if (element.needPopup) {
+                        boolean show = true;
+                        for (int i = 0; i < element.type.ordinal(); i++) {
+                            if (null == orderedQueue.get(i) || null != orderedQueue.get(i) && orderedQueue.get(i).needPopup && !orderedQueue.get(i).isPopup) {
+                                show = false;
+                                break;
+                            }
+                        }
+                        if (element.needPopup && show) {
+                            popupListener.onPopup(element);
+                        }
+//                    }
+//                    else {
+//                        dequeue(element.type);
+//                    }
                 }
             }
         } else {//非顺序弹窗需要弹出时添加
-            synchronized (unOrderExecuteList) {
-                unOrderExecuteList.add(element);
-                if (canUnOrderExe && currentElement == null) {
-                    popupUnOrder();
+            if (!exists(element.type)) {
+                synchronized (unorderedQueue) {
+                    unorderedQueue.add(element);
+                    if (canUnorderedPopup && currentElement == null) {
+                        popupUnOrder();
+                    }
                 }
             }
         }
@@ -84,16 +105,29 @@ public class DialogQueue {
      *
      * @param type 弹窗类型
      */
-    public void finish(Type type) {
+    public void dequeue(Type type) {
         if (type.isOrder) {//顺序弹窗关闭
-            synchronized (orderExecuteList) {
-                orderExecuteList.remove(currentElement);
-                currentElement = null;
-                popupOrder();
+            if (orderedQueue.size() > type.ordinal()) {
+                synchronized (orderedQueue) {
+                    Element element = orderedQueue.get(type.ordinal());
+                    if (null != element) element.isPopup = true;
+                    for (int i = type.ordinal() + 1; i < orderedCount; i++) {
+                        if (null == orderedQueue.get(i)) return;
+                        if (orderedQueue.get(i).needPopup && !orderedQueue.get(i).isPopup) {
+                            popupListener.onPopup(orderedQueue.get(i));
+                            return;
+                        }
+                    }
+                    for (Element e : orderedQueue) {
+                        if (e == null) return;//后边还有未请求回来的顺序弹窗
+                    }
+                    canUnorderedPopup = true;
+                    popupUnOrder();
+                }
             }
         } else {//非顺序弹窗关闭
-            synchronized (unOrderExecuteList) {
-                unOrderExecuteList.remove(currentElement);
+            synchronized (unorderedQueue) {
+                unorderedQueue.remove(currentElement);
                 currentElement = null;
                 popupUnOrder();
             }
@@ -101,49 +135,22 @@ public class DialogQueue {
     }
 
     /**
-     * 顺序弹窗不需要弹出时移除
-     */
-    public void deleteOrderExe() {
-        synchronized (orderExecuteList) {
-            orderExecuteNum--;
-            if (orderExecuteList.size() == orderExecuteNum) {
-                Collections.sort(orderExecuteList, comparator);
-                popupOrder();
-            }
-        }
-    }
-
-    /**
-     * 顺序弹窗弹出
-     */
-    private void popupOrder() {
-        if (orderExecuteList.size() > 0) {
-            currentElement = orderExecuteList.get(0);
-            popUpListener.onPopUp(currentElement);
-        } else {
-            canUnOrderExe = true;
-            popupUnOrder();
-        }
-    }
-
-
-    /**
      * 非顺序弹窗弹出
      */
     private void popupUnOrder() {
-        if (unOrderExecuteList.size() > 0) {
-            currentElement = unOrderExecuteList.get(0);
-            popUpListener.onPopUp(currentElement);
+        if (unorderedQueue.size() > 0) {
+            currentElement = unorderedQueue.get(0);
+            popupListener.onPopup(currentElement);
         }
     }
 
     /**
      * 判断非顺序弹窗里是否已有type类型的元素
      */
-    public boolean hasType(Type type) {
-        if (unOrderExecuteList.size() > 0) {
-            for (int i = 0; i < unOrderExecuteList.size(); i++) {
-                if (unOrderExecuteList.get(i).type.orderValue == type.orderValue) {
+    private boolean exists(Type type) {
+        if (unorderedQueue.size() > 0) {
+            for (int i = 0; i < unorderedQueue.size(); i++) {
+                if (unorderedQueue.get(i).type.ordinal() == type.ordinal()) {
                     return true;
                 }
             }
@@ -151,54 +158,34 @@ public class DialogQueue {
         return false;
     }
 
-    class OrderListComparator implements Comparator<Element> {
-        @Override
-        public int compare(Element o1, Element o2) {
-            return o1.type.orderValue - o2.type.orderValue;
-        }
-    }
-
     /**
      * 弹出弹窗监听
      */
-    public interface PopUpListener {
-        void onPopUp(Element element);
-    }
-
-    /**
-     * 弹窗类型
-     */
-    public class Type {
-        public String name;
-        public boolean isOrder;
-        public int orderValue;
-
-        public Type(String name, boolean isOrder, int orderValue) {
-            this.name = name;
-            this.isOrder = isOrder;
-            this.orderValue = orderValue;
-        }
-
+    public interface PopupListener {
+        void onPopup(Element element);
     }
 
     /**
      * 弹窗元素
      */
     public static class Element {
-        private Type type;
-        private Object data;
+        public Type type;//弹窗类型
+        public boolean needPopup = true;//弹窗是否需要弹出
+        public Object data;//弹窗数据
+        public boolean isPopup;//弹窗是否已经弹出过
+
+        /**
+         * 顺序弹窗不需要弹窗是调用此构造方法添加元素,needPop传入false,其他情况均使用{@link Element#Element(Type, Object)}
+         */
+        public Element(Type type, boolean needPopup, Object data) {
+            this.type = type;
+            this.needPopup = needPopup;
+            this.data = data;
+        }
 
         public Element(Type type, Object data) {
             this.type = type;
             this.data = data;
-        }
-
-        public Type getType() {
-            return type;
-        }
-
-        public Object getData() {
-            return data;
         }
     }
 }
